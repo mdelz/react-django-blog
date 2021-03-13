04c8090284b64b1215c5f3f67a432e5769462b2f
Michael Delz <mdelz@mintel.com>
2021-03-13 12:58:52 +0000

    Add NavBar files

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Link, Route, Switch } from "wouter";
+
+class NavBar extends Component {
+  render() {
+    return <h1>Placeholder</h1>;
+  }
+}
+
+export default NavBar;
